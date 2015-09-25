837549a70b36e945e1e0834d6d7e827e917c1417
jaredlang <jaredlang76@gmail.com>
2015-09-25 16:09:13 -0500

    implemented all query methods in QueryMap widget
    rectangle, polygon and point buffer (with esri./toolbars/draw)

--- dojo/js/apc/dijit/QueryMap.js
+++ dojo/js/apc/dijit/QueryMap.js
@@ -14,6 +14,7 @@ define([
     "dojo/dom-style",
     "dojo/ready",
 
+	"esri/toolbars/draw", 
     "esri/layers/GraphicsLayer", 
 	"esri/graphic",
     "esri/geometry/Circle",
@@ -30,7 +31,7 @@ define([
     Evented, declare, lang, array, 
     parser, _TemplatedMixin,
     on, dom, domConstruct, domClass, domStyle, ready, 
-    GraphicsLayer, Graphic, Circle, Color, 
+    Draw, GraphicsLayer, Graphic, Circle, Color, 
     SimpleMarkerSymbol, SimpleLineSymbol, SimpleFillSymbol, SimpleRenderer, 
     dijitTemplate
 ) {
@@ -43,7 +44,7 @@ define([
             map: null, // required
             title: "Map Query",
             symbols: {
-				"point": {
+				"marker": {
 					"type": "esriSMS",
 					"style": "esriSMSCross",
 					"color": [0,0,255,255],
@@ -62,7 +63,7 @@ define([
 					"color": [0,0,255,255],
 					"width": 2
 				},
-				"polygon": {
+				"fill": {
 					"type": "esriSFS",
 					"style": "esriSFSSolid",
 					"color": [0,0,0,75],
@@ -88,8 +89,11 @@ define([
         _queryMethod: "rectangle", 
         _distanceUnit: "esriFeet",  
 
-        _queryMapHandler: null, 
-
+		// mouse event handlers for query
+        _queryMapHandlers: [], 
+		_drawToolbar: null, 
+		
+		// graphic layer for query
         _queryLayerId: "queryMap_graphicsLayer",
         _queryLayer: null, 
 
@@ -128,8 +132,11 @@ define([
         // connections/subscriptions will be cleaned up during the destroy() lifecycle phase
         destroy: function () {
             this.inherited(arguments);
+			// reset the draw toolbar 
+			this._drawToolbar.deactivate(); 
+			this._drawToolbar = null; 
             // reset the query drawing
-			this._resetQueryDrawing(); 
+			this._resetQueryDrawing();
 			// remove the graphic layer from map 
 			if (this._queryLayer) {
 				this.map.removeLayer(this._queryLayer);
@@ -145,7 +152,12 @@ define([
         _init: function () {
         	// init the graphic layer
 			this._queryLayer = new GraphicsLayer({id: this._queryLayerId}); 
-			this.map.addLayer(this._queryLayer);         	
+			this.map.addLayer(this._queryLayer);			
+			// init the draw toolbar 
+			this._drawToolbar = new Draw(this.map);
+			this._drawToolbar.setMarkerSymbol(new SimpleMarkerSymbol(this.symbols["marker"]));
+			this._drawToolbar.setLineSymbol(new SimpleMarkerSymbol(this.symbols["line"]));
+			this._drawToolbar.setFillSymbol(new SimpleFillSymbol(this.symbols["fill"]));
         	// init the query drawing
         	this._prepareQueryDrawing(); 
         	// 
@@ -170,43 +182,79 @@ define([
             this._status.innerHTML = message;
         }, 
 
-        _resetQueryDrawing: function() {
-			this._queryMethod = null; 
-			if (this._queryMapHandler) {
-				this._queryMapHandler.remove();
-			}
+        _resetQueryDrawing: function() {			
+			this.showMessage(""); 
+			
+			// deactivate the draw toolbar 
+			this._drawToolbar.deactivate(); 
+			// remove all graphics
 			if (this._queryLayer) {
 				this._queryLayer.clear(); 
 			}
+			// reset the query method
+			this._queryMethod = null;
+			// remove the drawing event handler
+			if (this._queryMapHandlers) {
+				array.forEach(this._queryMapHandlers, function(evtHandler) {
+					evtHandler.remove();
+				}); 
+			}
+			this._queryMapHandlers = []; 
         }, 
 
         _prepareQueryDrawing: function() {
+			this.showMessage(""); 
+			
+			// display the pointBuffer options
 			domStyle.set(this._bufferOptions, "display", (this._queryMethod === "pointBuffer"?'block':"none")); 
 
-			var queryGeometry; 
+			// bind on mouse events on map
 			switch(this._queryMethod) {
 				case "rectangle":
+					this._queryMapHandlers.push(
+						this._drawToolbar.on("draw-end", lang.hitch(this, function (evt) {
+							// clear previous drawing
+							this._queryLayer.clear();
+							// draw the polygon
+							this._queryLayer.add(new Graphic(evt.geometry, new SimpleFillSymbol(this.symbols["fill"])));
+							// execute the query
+							this._doQuery(evt.geometry); 
+						}))
+					);
+					this._drawToolbar.activate(Draw.EXTENT);
 					break; 
 				case "polygon":
+					this._queryMapHandlers.push(
+						this._drawToolbar.on("draw-end", lang.hitch(this, function (evt) {
+							// clear previous drawing
+							this._queryLayer.clear();
+							// draw the polygon
+							this._queryLayer.add(new Graphic(evt.geometry, new SimpleFillSymbol(this.symbols["fill"])));
+							// execute the query
+							this._doQuery(evt.geometry); 
+						}))
+					);
+					this._drawToolbar.activate(Draw.POLYGON);
 					break; 
 				case "pointBuffer":
-					this._queryMapHandler = on(this.map, "click", lang.hitch(this, function (evt) {
-			        	this.showMessage(""); 
-						if (/^\d+$/.test(this._bufferDistance.value) === false) {
-							this.showMessage("invalid distance input");
-						} else {
-							queryGeometry = new Circle({
-								center: evt.mapPoint,
-								geodesic: true,
-								radius: this._bufferDistance.value,
-								radiusUnit: this._distanceUnit
-							}); 
-							this._doQuery(queryGeometry); 
-
-							this._queryLayer.add(new Graphic(evt.mapPoint, new SimpleMarkerSymbol(this.symbols["point"]))); 
-							this._queryLayer.add(new Graphic(queryGeometry, new SimpleFillSymbol(this.symbols["polygon"]))); 
-						}
-					}));
+					this._queryMapHandlers.push(
+						this._drawToolbar.on("draw-end", lang.hitch(this, function (evt) {
+								// clear previous drawing
+								this._queryLayer.clear();
+								// draw the buffer circle
+								var queryGeometry = new Circle({
+										center: evt.geometry,
+										geodesic: true,
+										radius: this._bufferDistance.value,
+										radiusUnit: this._distanceUnit
+									});
+								this._queryLayer.add(new Graphic(evt.geometry, new SimpleMarkerSymbol(this.symbols["marker"]))); 
+								this._queryLayer.add(new Graphic(queryGeometry, new SimpleFillSymbol(this.symbols["fill"]))); 
+								// execute the query
+								this._doQuery(queryGeometry); 
+						}))
+					);
+					this._drawToolbar.activate(Draw.POINT);
 			}
         }, 
 
